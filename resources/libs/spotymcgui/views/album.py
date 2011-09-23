e93dbe143039205a599e38d803333e981f4bcf55
Mikel Azkolain <azkotoki@gmail.com>
2011-09-23 23:34:37 +0200

    spotymcgui: views: album: some Dharma fixes on the album browsing.

--- resources/libs/spotymcgui/views/album.py
+++ resources/libs/spotymcgui/views/album.py
@@ -42,27 +42,27 @@ class AlbumTracksView(BaseView):
         self.__album = album
     
     
-    def _play_selected_track(self, window):
+    def _play_selected_track(self, view_manager, window):
+        #print 'inside play_selected track'
         pos = self._get_list(window).getSelectedPosition()
         track_item = self.__albumbrowse.track(pos)
-        track_link = link.create_from_track(track_item)
-        track_id = track_link.as_string()[14:]
-        p = xbmc.Player(xbmc.PLAYER_CORE_MPLAYER)
-        p.play("http://localhost:8080/track/%s.wav" % track_id)
-        
+        playlist_manager = view_manager.get_var('playlist_manager')
+        playlist_manager.play(track_item)
     
     
     def click(self, view_manager, window, control_id):
+        print "list clicked: id: %d" % control_id
         if control_id == AlbumTracksView.__list_id:
-            self._play_selected_track(window)
+            print 'control id test ok'
+            self._play_selected_track(view_manager, window)
     
     
     def _get_list(self, window):
         return window.getControl(AlbumTracksView.__list_id)
     
     
-    def _add_track(self, list, title, path, duration, number):
-        item = xbmcgui.ListItem(path=path)
+    def _add_track(self, list, title, item_path, duration, number):
+        item = xbmcgui.ListItem(path=item_path)
         item.setInfo(
             "music",
             {"title": title, "duration": duration, "tracknumber": number}
@@ -75,8 +75,11 @@ class AlbumTracksView(BaseView):
         l.reset()
         
         for item in albumbrowse.tracks():
+            track_link = link.create_from_track(item)
+            track_id = track_link.as_string()[14:]
+            track_url = "http://localhost:8080/track/%s.wav" % track_id
             self._add_track(
-                l, item.name(), "", item.duration() / 1000, item.index()
+                l, item.name(), track_url, item.duration() / 1000, item.index()
             )
         
         album = albumbrowse.album()
@@ -92,10 +95,8 @@ class AlbumTracksView(BaseView):
         self.__albumbrowse = albumbrowse.Albumbrowse(self.__session, self.__album, cb)
         c = window.getControl(AlbumTracksView.__group_id)
         c.setVisibleCondition("true")
-        print "show!"
     
     
     def hide(self, window):
         c = window.getControl(AlbumTracksView.__group_id)
         c.setVisibleCondition("false")
-        print "hide!"
