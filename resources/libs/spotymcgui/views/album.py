4c065673903e8e69d55ce5fd4bed8221dba0f5d3
Mikel Azkolain <azkotoki@gmail.com>
2011-10-03 19:05:24 +0200

    Album view: Implemented disc grouping.

--- resources/libs/spotymcgui/views/album.py
+++ resources/libs/spotymcgui/views/album.py
@@ -47,16 +47,19 @@ class AlbumTracksView(BaseView):
     
     def _play_selected_track(self, view_manager, window):
         #print 'inside play_selected track'
-        pos = self._get_list(window).getSelectedPosition()
-        track_item = self.__albumbrowse.track(pos)
-        playlist_manager = view_manager.get_var('playlist_manager')
-        playlist_manager.play(track_item)
+        #pos = self._get_list(window).getSelectedPosition()
+        item = self._get_list(window).getSelectedItem()
+        pos = item.getProperty("real_index")
+        
+        #If we have a valid index
+        if pos is not None:
+            track_item = self.__albumbrowse.track(int(pos))
+            playlist_manager = view_manager.get_var('playlist_manager')
+            playlist_manager.play(track_item)
     
     
     def click(self, view_manager, window, control_id):
-        print "list clicked: id: %d" % control_id
         if control_id == AlbumTracksView.list_id:
-            print 'control id test ok'
             self._play_selected_track(view_manager, window)
     
     
@@ -64,33 +67,61 @@ class AlbumTracksView(BaseView):
         return window.getControl(AlbumTracksView.list_id)
     
     
-    def _add_track(self, list, title, item_path, duration, number):
-        item = xbmcgui.ListItem(path=item_path)
-        item.setInfo(
-            "music",
-            {"title": title, "duration": duration, "tracknumber": number}
-        )
+    def _have_multiple_discs(self, track_list):
+        for item in track_list:
+            if item.disc() > 1:
+                return True
+        
+        return False
+    
+    
+    def _set_album_info(self, window, album, artist):
+        window.setProperty("AlbumCover", "http://localhost:8080/image/%s.jpg" % album.cover())
+        window.setProperty("AlbumName", album.name())
+        window.setProperty("ArtistName", artist.name())
+    
+    
+    def _add_disc_separator(self, list, disc_number):
+        item = xbmcgui.ListItem()
+        item.setProperty("IsDiscSeparator", "true")
+        item.setProperty("DiscNumber", str(disc_number))
+        list.addItem(item)
+    
+    def _add_track(self, list, track, real_index):
+        track_link = link.create_from_track(track)
+        track_id = track_link.as_string()[14:]
+        track_url = "http://localhost:8080/track/%s.wav" % track_id
+        
+        item = xbmcgui.ListItem(path=track_url)
+        info = {
+            "title": track.name(),
+            "duration": track.duration() / 1000,
+            "tracknumber": track.index(),
+        }
+        
+        item.setProperty("real_index", str(real_index))
+        item.setInfo('music', info)
         list.addItem(item)
     
     
     def populate_list(self, window, albumbrowse):
-        l = self._get_list(window)
-        l.reset()
+        list = self._get_list(window)
+        list.reset()
         
-        for item in albumbrowse.tracks():
-            track_link = link.create_from_track(item)
-            track_id = track_link.as_string()[14:]
-            track_url = "http://localhost:8080/track/%s.wav" % track_id
-            self._add_track(
-                l, item.name(), track_url, item.duration() / 1000, item.index()
-            )
+        #Set album info
+        self._set_album_info(window, albumbrowse.album(), albumbrowse.artist())
         
-        album = albumbrowse.album()
-        artist = albumbrowse.artist()
+        #For disc grouping
+        multiple_discs = self._have_multiple_discs(albumbrowse.tracks())
+        last_disc = None
         
-        window.setProperty("AlbumCover", "http://localhost:8080/image/%s.jpg" % album.cover())
-        window.setProperty("AlbumName", album.name())
-        window.setProperty("ArtistName", artist.name())
+        for idx, item in enumerate(albumbrowse.tracks()):
+            #If disc was changed add a separator
+            if multiple_discs and last_disc != item.disc():
+                last_disc = item.disc()
+                self._add_disc_separator(list, last_disc)
+            
+            self._add_track(list, item, idx)
         
     
     def show(self, window):
