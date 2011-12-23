22e9ee67a4d228bab119ce5bda921f9a0e4d3a31
Mikel Azkolain <azkotoki@gmail.com>
2011-12-23 20:25:15 +0100

    Revamped envutils and added Mac support.

--- envutils.py
+++ envutils.py
@@ -1,30 +1,30 @@
 import struct, os, sys
 
 
+def get_platform_path():
+    arch = struct.calcsize("P") * 8
+    
+    if os.name == "nt":
+        if arch == 32:
+            return 'windows/x86'
+            
+        elif arch == 64:
+            raise OSError('Windows x86_64 is not supported.')
+    
+    elif os.name == "posix":
+        if sys.platform.startswith('linux'):
+            if arch == 32:
+                return 'linux/x86'
+            
+            elif arch == 64:
+                return 'linux/x86_64'
+        
+        elif sys.platform == 'darwin':
+            return 'osx'
+
 
 def set_library_path(root):
-    absroot = os.path.abspath(root)
-    
-    
-    #Platform specific paths
-    if os.name == "nt":
-        plat_chunk = "win32"
-    
-    #linux == posix, i promise that it's a quirk, not intentional!
-    elif os.name == "posix":
-        plat_chunk = "linux"
-    
-    
-    #Arch specific paths
-    #void pointer size, 32 or 64
-    arch = struct.calcsize("P") * 8
-    if arch == 32:
-        arch_chunk = "x86"
-    else:
-        arch_chunk = "x86_64"
-    
-    
     #Build the full path and publish it
-    full_path = os.path.join(absroot, plat_chunk, arch_chunk)
+    full_path = os.path.join(os.path.abspath(root), get_platform_path())
     os.environ["PATH"] = full_path + ";" + os.environ["PATH"]
     sys.path.append(full_path)
