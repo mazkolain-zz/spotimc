c621dde87aef6df78c692308ea8e0c9448445d02
Mikel Azkolain <azkotoki@gmail.com>
2011-12-31 11:13:05 +0100

    Search View: Updated skin layout and controller.

--- resources/libs/spotymcgui/views/search.py
+++ resources/libs/spotymcgui/views/search.py
@@ -4,20 +4,25 @@ Created on 22/08/2011
 @author: mikel
 '''
 import xbmcgui
-from spotymcgui.views import BaseView
+from spotymcgui.views import BaseListContainerView
 
 
-class SearchTracksView(BaseView):
-    __group_id = 1500
-    __list_id = 1501
+
+class SearchTracksView(BaseListContainerView):
+    container_id = 1500
+    list_id = 1520
     
     
-    def click(self, view_manager, window, control_id):
+    def click(self, view_manager, control_id):
         pass
     
     
-    def _get_list(self, window):
-        return window.getControl(SearchTracksView.__list_id)
+    def get_container(self, view_manager):
+        return view_manager.get_window().getControl(SearchTracksView.container_id)
+    
+    
+    def get_list(self, view_manager):
+        return view_manager.get_window().getControl(SearchTracksView.list_id)
     
     
     def _add_track(self, list, title, artist, album, path, duration):
@@ -31,8 +36,10 @@ class SearchTracksView(BaseView):
         )
         list.addItem(item)
     
-    def _populate_list(self, window):
-        l = self._get_list(window)
+    
+    def render(self, view_manager):
+        window = view_manager.get_window()
+        l = self.get_list(view_manager)
         l.reset()
         
         self._add_track(l, "Track 1", "Artist1", "Album1", "", 186)
@@ -47,22 +54,17 @@ class SearchTracksView(BaseView):
         self._add_track(l, "Track 1", "Artist1", "Album1", "", 7560)
         self._add_track(l, "A very long track name that overflows the column", "Huey Lewis and the News", "A long album name that overflows the column", "", 256)
         self._add_track(l, "Track 1", "Artist1", "Album1", "", 256)
+        self._add_track(l, "Track 1", "Artist1", "Album1", "", 256)
+        self._add_track(l, "Track 1", "Artist1", "Album1", "", 256)
+        self._add_track(l, "Track 1", "Artist1", "Album1", "", 256)
+        self._add_track(l, "Track 1", "Artist1", "Album1", "", 256)
         
         window.setProperty("SearchQuery", "Rick Astley")
+        window.setProperty("SearchDidYouMeanStatus", "true")
+        window.setProperty("SearchDidYouMeanString", "Rickrolled")
+        
+        return True
         #window.setProperty("AlbumCover", "http://www.me-pr.co.uk/axxis%20doom%20cover%20small.jpg")
         #window.setProperty("AlbumName", "Album Name")
         #window.setProperty("ArtistName", "Artist Name")
         #img.setImage("http://www.necramonium.com/photos/KISS-Album-Covers/cover_destroyer.jpg")
-        
-    
-    def show(self, window):
-        self._populate_list(window)
-        c = window.getControl(SearchTracksView.__group_id)
-        c.setVisibleCondition("true")
-        print "show!"
-    
-    
-    def hide(self, window):
-        c = window.getControl(SearchTracksView.__group_id)
-        c.setVisibleCondition("false")
-        print "hide!"
