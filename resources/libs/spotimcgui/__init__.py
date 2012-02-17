15a7325f26a56147c40f90b1588b1210f0bd8972
Mikel Azkolain <azkotoki@gmail.com>
2012-02-17 18:41:25 +0100

    Lots of legal stuff.

--- resources/libs/spotimcgui/__init__.py
+++ resources/libs/spotimcgui/__init__.py
@@ -1,8 +1,23 @@
 '''
-Created on 24/06/2011
+Copyright 2011 Mikel Azkolain
 
-@author: mikel
+This file is part of Spotymc.
+
+Spotymc is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Spotymc is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Spotymc.  If not, see <http://www.gnu.org/licenses/>.
 '''
+
+
 __all__ = ["mainwindow"]
 
 
