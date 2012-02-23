e06bc2bfc27cd342de519da1b059c9d8704cf6fd
Mikel Azkolain <azkotoki@gmail.com>
2012-02-23 14:51:28 +0100

    More name change issues.

--- resources/libs/spotimcgui/views/search.py
+++ resources/libs/spotimcgui/views/search.py
@@ -1,20 +1,20 @@
 '''
 Copyright 2011 Mikel Azkolain
 
-This file is part of Spotymc.
+This file is part of Spotimc.
 
-Spotymc is free software: you can redistribute it and/or modify
+Spotimc is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Spotymc is distributed in the hope that it will be useful,
+Spotimc is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Spotymc.  If not, see <http://www.gnu.org/licenses/>.
+along with Spotimc.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 
